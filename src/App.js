33fe928481a9c7600766e71b1a887178b84303fc
Anthony <alasam@live.com>
2022-11-18 18:03:41 -0500

    move items and function to app.js, mapped into cards component as props:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import Cards from "./components/Cards";
 
 function App() {
   const { count, setCount } = useState(0);
+  const items = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {items.map((number, i) => (
+        <Cards key={i} number={number} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-function cards() {
-  const items = ["1", "2", "3", "4", "5", "6", "7", "8"];
-
-  return <div>cards</div>;
+function Cards({ number }) {
+  return <div>{number}</div>;
 }
 
-export default cards;
+export default Cards;
