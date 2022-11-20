4a86196080a95575030820071a6adf6db38db99b
Anthony <alasam@live.com>
2022-11-20 03:50:17 -0500

    add front of card, made cards clickable buttons

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import cardFront from "../assets/cardfront.webp";
 
 function Cards({ number }) {
-  return <div>{number}</div>;
+  return (
+    // Card Container
+    <button className="grid grid-cols-4">
+      {/* front */}
+      <div>
+        <img src={cardFront} alt="front" width="60" />
+      </div>
+      {/* //Card */}
+      <div className="w-[100px] h-[100px] text-5xl">{number}</div>
+    </button>
+  );
 }
 
 export default Cards;
