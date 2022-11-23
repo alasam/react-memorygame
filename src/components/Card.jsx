3c04c9c5bd41e7acbad4f499a2f2a1ca5de8c810
Anthony <alasam@live.com>
2022-11-23 02:43:50 -0500

    onclick to see card id

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import cardFront from "../assets/cardfront.png";
 
-function Card({ name, pokemon }) {
-  const cardClick = function () {
-    console.log("click", +name);
+function Card({ name, pokemon, id }) {
+  const cardClick = function (e) {
+    console.log(e.target.dataset.id);
   };
 
   return (
@@ -11,9 +11,10 @@ function Card({ name, pokemon }) {
     <button
       className="grid w-[150px] h-[150px] place-items-center bg-gray-400 border-2 cursor-pointer"
       onClick={cardClick}
+      data-id={id}
     >
       {/* front */}
-      <div>
+      <div className="pointer-events-none">
         <img src={cardFront} alt="front" className="border-2 w-14" />
       </div>
       {/* //Card */}
@@ -22,7 +23,7 @@ function Card({ name, pokemon }) {
         alt="pokemon"
         width="200"
         height="20"
-        className="border-2 w-14"
+        className="border-2 w-14 pointer-events-none"
       />
       <p className=" text-sm">{name}</p>
     </button>
