871277187f4d4f73589214356be42aaf7898042d
Anthony <alasam@live.com>
2022-11-23 01:48:39 -0500

    created funtion to double and randomize eeveelution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Espeon from "./assets/Espeon.webp";
 import Flareon from "./assets/Flareon.png";
 import Glaceon from "./assets/Glaceon.png";
@@ -10,25 +10,37 @@ import Vaporeon from "./assets/Vaporeon.png";
 import Cards from "./components/Cards";
 
 function App() {
-  const { count, setCount } = useState(0);
+  const [Cards, setCards] = useState(null);
   const eeveelution = [
-    { name: "Espeon", pokemon: Espeon },
-    { name: "Flareon", pokemon: Flareon },
-    { name: "Glaceon", pokemon: Glaceon },
-    { name: "Jolteon", pokemon: Jolteon },
-    { name: "Leafeon", pokemon: Leafeon },
-    { name: "Sylveon", pokemon: Sylveon },
-    { name: "Umbreon", pokemon: Umbreon },
-    { name: "Vaporeon", pokemon: Vaporeon },
+    { key: "1", name: "Espeon", pokemon: Espeon },
+    { key: "2", name: "Flareon", pokemon: Flareon },
+    { key: "3", name: "Glaceon", pokemon: Glaceon },
+    { key: "4", name: "Jolteon", pokemon: Jolteon },
+    { key: "5", name: "Leafeon", pokemon: Leafeon },
+    { key: "6", name: "Sylveon", pokemon: Sylveon },
+    { key: "7", name: "Umbreon", pokemon: Umbreon },
+    { key: "8", name: "Vaporeon", pokemon: Vaporeon },
   ];
+
+  function resetGame() {
+    const shuffled = [...eeveelution, ...eeveelution]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, key: Math.random() }));
+    setCards(shuffled);
+    console.log(Object.entries(shuffled));
+  }
+
+  useEffect(() => {
+    resetGame();
+  }, []);
 
   return (
     <div className="grid place-items-center">
-      <div className="grid grid-cols-4">
-        {eeveelution.map((eeveelution) => (
-          <Cards eeveelution={eeveelution} />
+      {/* <div className="grid grid-cols-4">
+        {Object.entries(eeveelution).map((eeveelution) => (
+          <Cards key={eeveelution.key} eeveelution={eeveelution} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
