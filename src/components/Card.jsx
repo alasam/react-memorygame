c8a8a3363c7184d9b5eb666effe212580ccb8e53
Anthony <alasam@live.com>
2022-11-29 17:12:24 -0500

    add card clipping styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,8 @@ import cardFront from "../assets/cardfront.png";
 function Card({ card, cardClick, disabled, index }) {
   return (
     // Card Container
-    <button
-      className={`flex w-[150px] h-[150px] justify-between cursor-pointer ${
+    <div
+      className={`flex w-[150px] h-[150px] justify-between cursor-pointer group perspective ${
         card.matched || disabled
           ? "bg-gray-400 pointer-events-none"
           : "bg-blue-400"
@@ -15,18 +15,26 @@ function Card({ card, cardClick, disabled, index }) {
       disabled={disabled ? true : false}
       data-index={index}
     >
-      {/* front */}
-      <div className="pointer-events-none">
-        <img src={cardFront} alt="front" className="absolute w-[140px] " />
-
-        {/* //Card */}
-        <img
-          src={card.pokemon}
-          alt="pokemon"
-          className="absolute w-[140px] pointer-events-none"
-        />
+      {/* Card */}
+      <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+        {/* Front */}
+        <div className="absolute backface-hidden border-2 w-full h-full">
+          <img
+            src={cardFront}
+            alt="front"
+            className="absolute w-full h-full "
+          />
+        </div>
+        {/* Back */}
+        <div className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-black">
+          <img
+            src={card.pokemon}
+            alt="pokemon"
+            className="absolute w-full h-full pointer-events-none "
+          />
+        </div>
       </div>
-    </button>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   varients: {},
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".my-rotate-y-180": {
+          transform: "rotateY(180deg)",
+        },
+        ".preserve-3d": {
+          transformStyle: "preserve-3d",
+        },
+        ".perspective": {
+          perspective: "1000px",
+        },
+        ".backface-hidden": {
+          backfaceVisibility: "hidden",
+        },
+      });
+    }),
+  ],
 };
